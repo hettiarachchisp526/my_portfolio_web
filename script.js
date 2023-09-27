3cccbe2757bd403c7cd238cfdc62487a731f434c
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2023-09-27 17:07:13 +0530

    update-second

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+/*nav bar showing*/
 const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+
